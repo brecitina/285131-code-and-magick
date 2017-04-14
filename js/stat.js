be993f69a8863e59aeeb5ca957e1eff16f00ac93
Alina Volkova <brecit@yandex.ru>
2017-04-14 23:25:20 +0400

    add spaces and delete indexOfMaxTime

--- js/stat.js
+++ js/stat.js
@@ -1,34 +1,32 @@
 'use strict';
 function renderStatistics(ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270); //Тень
+  ctx.fillRect(110, 20, 420, 270); // Тень
 
   ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, 420, 270); //Лист статистики
+  ctx.fillRect(100, 10, 420, 270); // Лист статистики
 
   ctx.fillStyle = 'red';
   ctx.font = '16px PT Mono'; 
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  var i; //индекс для массивов
+  var i; // индекс для массивов
   var maxTime = -1;
-  var indexOfMaxTime;
   
   for (i = 0; i < times.length; i++) {
     if (times[i] > maxTime) {
       maxTime = times[i];
-      indexOfMaxTime = i;
     };
   };
 
   var histogramHeight = 150;
-  var histogramUnitHeight = histogramHeight / maxTime; //Высота единицы для пропорционального отображения
+  var histogramUnitHeight = histogramHeight / maxTime; // Высота единицы для пропорционального отображения
   var histogramWidth = 40;
   var histogramIndent = 90;
   var textIndent = 10;
-  var inintialX = 155; //Начальная координата гистрограммы по Х
-  var inintialУ = 90; //Начальная координата гистрограммы по У 
+  var inintialX = 155; // Начальная координата гистрограммы по Х
+  var inintialУ = 90; //  Начальная координата гистрограммы по У 
 
   for (i = 0; i < times.length; i++) {
     (names[i] === 'Вы') ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random()*(1 - 0.1) + 0.1) + ')';
